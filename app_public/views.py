93904a11ed9517c400f0f6b0ad6c6b1750b4ad16
Kauan Henrique <khds2013@gmail.com>
2024-11-07 17:42:34 -0300

    Teste

--- app_public/views.py
+++ app_public/views.py
@@ -105,6 +105,7 @@ def create_company(request):
 
     
 def edit(request):
+    # 
     data = json.loads(request.body)
     company = Company.objects.get(id=data['id'])
     serializer = CompanySerializer(company, data=data)
